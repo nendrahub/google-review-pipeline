e861483ce5e33e8afc5504fc955bc2f1d07e1d0b
Danendra Sajana Dhika <150692682+nendrahub@users.noreply.github.com>
2026-07-25 08:44:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from apify_client import ApifyClient
 from supabase import create_client
+from datetime import datetime, timezone
 import os
 
 
@@ -27,7 +28,7 @@ run_input = {
     "language": "id",
     "personalData": True,
     "reviewsOrigin": "google",
-    "reviewsStartDate": "3 days",
+    "reviewsStartDate": "1 days",
     "startUrls": [
         {
             "url": "https://www.google.com/maps/place/Daya+Toyota+Cakung+Official/@-6.192028,106.970474,17z/data=!3m1!4b1!4m8!3m7!1s0x2e698b9e6c61a61f:0x5aced58296cf20ae!8m2!3d-6.192028!4d106.970474!9m1!1b1!16s%2Fg%2F11hdq83g_c"
@@ -36,11 +37,12 @@ run_input = {
 }
 
 
+print("START ACTOR")
+
 # jalankan Apify
 run = apify.actor(ACTOR_ID).call(
     run_input=run_input
 )
-
 
 print("Status:", run.status)
 
@@ -50,13 +52,17 @@ reviews = list(
     apify.dataset(run.default_dataset_id).iterate_items()
 )
 
-
 print("Jumlah review:", len(reviews))
 
 
 # kalau kosong berhenti
 if len(reviews) == 0:
+    print("Tidak ada review baru")
     exit()
+
+
+# waktu scrape sekarang
+now = datetime.now(timezone.utc).isoformat()
 
 
 # mapping sesuai tabel Supabase
@@ -76,15 +82,21 @@ for r in reviews:
         "review_url": r.get("reviewUrl"),
         "response_from_owner_date": r.get("responseFromOwnerDate"),
         "response_from_owner_text": r.get("responseFromOwnerText"),
-        "scraped_at": r.get("scrapedAt")
+
+        # selalu update setiap workflow berjalan
+        "scraped_at": now
 
     })
 
 
-# masuk Supabase
-supabase.table("reviews_raw")\
-    .upsert(data)\
+# upsert berdasarkan review_id
+supabase.table("reviews_raw") \
+    .upsert(
+        data,
+        on_conflict="review_id"
+    ) \
     .execute()
 
 
 print("Selesai upload:", len(data))
+print("Scraped at:", now)
