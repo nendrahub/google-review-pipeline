44dc27f4a3012ce3a1ef09ec19459b3a1a3a018d
Danendra Sajana Dhika <150692682+nendrahub@users.noreply.github.com>
2026-08-07 01:00:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ run_input = {
     "language": "id",
     "personalData": True,
     "reviewsOrigin": "google",
-    "reviewsStartDate": "1 days",
+    "reviewsStartDate": "2018-01-01",
     "startUrls": [
         {
             "url": "https://www.google.com/maps/place/Daya+Toyota+Cakung+Official/@-6.192028,106.970474,17z/data=!3m1!4b1!4m8!3m7!1s0x2e698b9e6c61a61f:0x5aced58296cf20ae!8m2!3d-6.192028!4d106.970474!9m1!1b1!16s%2Fg%2F11hdq83g_c"
@@ -72,25 +72,26 @@ for r in reviews:
 
     data.append({
 
-        "review_id": r.get("reviewId"),
-        "reviewer_id": r.get("reviewerId"),
-        "name": r.get("name"),
-        "text": r.get("text"),
-        "text_translated": r.get("textTranslated"),
-        "stars": r.get("stars"),
-        "published_at_date": r.get("publishedAtDate"),
-        "review_url": r.get("reviewUrl"),
-        "response_from_owner_date": r.get("responseFromOwnerDate"),
-        "response_from_owner_text": r.get("responseFromOwnerText"),
+    "review_id": r.get("reviewId"),
+    "reviewer_id": r.get("reviewerId"),
+    "name": r.get("name"),
+    "text": r.get("text"),
+    "text_translated": r.get("textTranslated"),
+    "stars": r.get("stars"),
+    "published_at_date": r.get("publishedAtDate"),
+    "review_url": r.get("reviewUrl"),
+    "response_from_owner_date": r.get("responseFromOwnerDate"),
+    "response_from_owner_text": r.get("responseFromOwnerText"),
 
-        # selalu update setiap workflow berjalan
-        "scraped_at": now
+    "totalScore": r.get("totalScore"),
 
-    })
+    "scraped_at": now
+
+})
 
 
 # upsert berdasarkan review_id
-supabase.table("reviews_raw") \
+supabase.table("reviews_raw_2018") \
     .upsert(
         data,
         on_conflict="review_id"
